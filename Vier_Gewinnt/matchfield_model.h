9a4dd628be2eb68d20192097dff6d11346624fbe
Philipp Scholz <Philipp.Scholz@fhbi.onmicrosoft.com>
2021-05-03 15:52:42 +0200

    Im matchfield_model die Methoden ausprogrammiert, matchfield_view vorerst ausprogrammiert

--- Vier_Gewinnt/matchfield_model.h
+++ Vier_Gewinnt/matchfield_model.h
@@ -6,10 +6,12 @@ class matchfield_model
 {
 private:
 	
-	int m_x, m_y;
-	std::vector <std::vector<FieldState::FieldState>> m_field;
+	int m_x, m_y;												//Größe des Spielfeldes
+	std::vector <std::vector<FieldState::FieldState>> m_field;	//Spielfeld-Vektor vom Typ FieldState
 
 public:
+
+
 	matchfield_model();
 	matchfield_model(int, int);
 	~matchfield_model();
@@ -17,6 +19,12 @@ public:
 	bool make_entry(int, FieldState::FieldState);
 	FieldState::FieldState get_entry(int);
 	void clear_field();
-
+	int get_coordinateX(int);
+	int get_coordinateY(int);
+	int get_x() {
+		return m_x;
+	}
+	int get_y() {
+		return m_y;
+	}
 };
-
--- Vier_Gewinnt/matchfield_view.cpp
+++ Vier_Gewinnt/matchfield_view.cpp
@@ -1,4 +1,6 @@
 #include "matchfield_view.h"
+#include "matchfield_model.h"
+#include <iostream>
 
 matchfield_view::matchfield_view()
 {
@@ -8,6 +10,21 @@ matchfield_view::~matchfield_view()
 {
 }
 
-void matchfield_view::show_model()
+void matchfield_view::show_model()      //Ausgeben des aktuellen Spielfeldes an die Konsole
 {
+    
+
+    for (int i = 1; i < m_model->get_x(); i++) {
+        for (int j = 1; j < m_model->get_y(); j++) {
+            if (m_model->get_entry(i * j) == FieldState::FieldState::eEmpty) {
+                std::cout << '| ' << '\t';
+            }
+            if (m_model->get_entry(i * j) == FieldState::FieldState::ePlayer1) {
+                std::cout << '| x' << '\t';
+            }
+            if (m_model->get_entry(i * j) == FieldState::FieldState::ePlayer2) {
+                std::cout << '| o' << '\t';
+            }
+        }
+    }
 }
